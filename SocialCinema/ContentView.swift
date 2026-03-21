6df689059cbe5bbfecf7f52d1f4467b7f6baff1c
Evan Samano <esamano3@asu.edu>
2026-03-21 14:00:11 -0700

    Initial Commit

---
+++ SocialCinema/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SocialCinema
+//
+//  Created by Evan Samano on 3/21/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SocialCinema/SocialCinemaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialCinemaApp.swift
+//  SocialCinema
+//
+//  Created by Evan Samano on 3/21/26.
+//
+
+import SwiftUI
+
+@main
+struct SocialCinemaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
