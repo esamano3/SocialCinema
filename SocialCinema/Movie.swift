00d9dcc38331a2f8fa09224db4954c4cc52e1539
Evan Samano <esamano3@asu.edu>
2026-03-21 14:50:35 -0700

    +Movie
    Created a basic model for Movie, with starter parameters of title, genre, synopsis.

---
+++ SocialCinema/Movie.swift
@@ -0,0 +1,14 @@
+//
+//  Movie.swift
+//  SocialCinema
+//
+//  Created by Evan Samano on 3/21/26.
+//
+
+import Foundation
+
+struct Movie {
+    let title: String
+    let genre: String
+    let synopsis: String
+}
