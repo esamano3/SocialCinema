29364484d6c16b32907608e6106246d2836c323c
josephloveall <josephploveall>
2026-04-17 12:26:03 -0700

    Integration of API call

--- SocialCinema/MovieAPIService.swift
+++ SocialCinema/MovieAPIService.swift
@@ -10,7 +10,7 @@ import Foundation
 class MovieAPIService {
     
     private let baseURL = "https://api.themoviedb.org/3/search/movie"
-    private let token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiYzNlNDA1NzU1MmY4YjYxOWRiZmQ5NGRlYmJkZjFiMiIsIm5iZiI6MTc3NjI3OTgxOC4yMjg5OTk5LCJzdWIiOiI2OWRmZTEwYWM4M2JhNTkxZDUyZDk5Y2IiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.U4AGLLArU6W2IfdeaifL3_n3_e1nT0bkMtWg7L_kDJ0"
+    private let token = "dummyToken"
     // temporary for testing
     
     func fetchMovies(query: String, completion: @escaping (Result<[Movie], Error>) -> Void) {
